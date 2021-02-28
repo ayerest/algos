3549f650955c16991964d7a54dc2c1335b0a7576
ayerest <irisiseve@gmail.com>
2021-02-28 10:57:12 -0800

    adding babel and jest, set up first algo string reversal

---
+++ src/str-reversal/reverseString.js
@@ -0,0 +1,5 @@
+const reverseString = () => {
+  console.log("hi");
+};
+
+export default reverseString; 
---
+++ src/str-reversal/reverseString.test.js
@@ -0,0 +1,7 @@
+import reverseString from './reverseString';
+
+describe(('reverseString method'), () => {
+  it("Should reverse string", () => {
+  assert.equal(reverseString("What is up?"), "?pu si tahW");
+ });
+})
