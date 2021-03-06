e0fa679cbd6c3bfede1faa60676700d879f6d4d9
ayerest <irisiseve@gmail.com>
2021-03-06 13:57:17 -0800

    palindrome solved

--- src/palindrome/palindrome.test.js
+++ src/palindrome/palindrome.test.js
@@ -4,6 +4,7 @@ import palindrome from './palindrome';
 describe(('palindrome method'), () => {
   it("Should verify if a string is a palindrome", () => {
    expect(palindrome("What is up?")).toBe(false);
-   expect(palindrome('gohangasalamiimalasagnahog')).toBe(true);
+   expect(palindrome("bob")).toBe(true);
+   expect(palindrome("go hang a salami I'm a lasagna hog")).toBe(true);
  });
 })
