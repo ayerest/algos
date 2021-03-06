ba63e519848578f82cb9d8e2f2e38861c57f3a0c
ayerest <irisiseve@gmail.com>
2021-03-06 14:00:31 -0800

    another test

--- src/palindrome/palindrome.test.js
+++ src/palindrome/palindrome.test.js
@@ -5,6 +5,7 @@ describe(('palindrome method'), () => {
   it("Should verify if a string is a palindrome", () => {
    expect(palindrome("What is up?")).toBe(false);
    expect(palindrome("bob")).toBe(true);
+   expect(palindrome("this is it")).toBe(false);
    expect(palindrome("go hang a salami I'm a lasagna hog")).toBe(true);
  });
 })
