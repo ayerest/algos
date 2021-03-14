0fbad1951418663011030af70df9fe19f852d492
ayerest <irisiseve@gmail.com>
2021-03-14 14:04:53 -0700

    fizzbuzz solution

--- src/fizz-buzz/fizzBuzz.js
+++ src/fizz-buzz/fizzBuzz.js
@@ -1,5 +1,17 @@
 const fizzBuzz = (num) => {
-  console.log("what");
+  const newArray = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      newArray.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      newArray.push('Fizz');
+    } else if (i % 5 === 0) {
+      newArray.push('Buzz');
+    } else {
+      newArray.push(i)
+    }
+  }
+  return newArray;
 }
 
 export default fizzBuzz;
