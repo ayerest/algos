a88d71d431a54e8b40780f01b19475df96ab94cd
ayerest <irisiseve@gmail.com>
2021-03-20 15:58:47 -0700

    solved max char problem

--- src/max-char/maxChar.js
+++ src/max-char/maxChar.js
@@ -1,5 +1,23 @@
-const maxChar = () => {
-  console.log("hi");
+const maxChar = (string) => {
+  const myHash = {};
+  const noSpaceZone = string.trim().split('')
+  noSpaceZone.forEach((char) => {
+    if (myHash[char]) {
+      myHash[char] += 1
+    } else {
+      myHash[char] = 1
+    }
+  })
+  let max = 0;
+  let myChar = '';
+  noSpaceZone.forEach((char) => {
+    if (myHash[char] > max) {
+      max = myHash[char];
+      myChar = char; 
+    } 
+  })
+  return myChar;
+  
 }
 
 export default maxChar;
