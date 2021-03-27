92dc02131b8d6587612b187bed0ef18a720f547e
ayerest <irisiseve@gmail.com>
2021-03-27 09:42:53 -0700

    anagrams problem solved

--- src/anagrams/anagrams.js
+++ src/anagrams/anagrams.js
@@ -1,5 +1,7 @@
-const anagrams = () => {
-  console.log("hiii")
+const anagrams = (stringA, stringB) => {
+  const stringASorted = stringA.split('').sort().join('');
+  const stringBSorted = stringB.split('').sort().join('');
+  return stringASorted === stringBSorted
 }
 
 export default anagrams;
--- src/anagrams/anagrams.test.js
+++ src/anagrams/anagrams.test.js
@@ -3,5 +3,7 @@ import anagrams from './anagrams';
 describe('anagrams method', () => {
   it('should check if a string contains the same characters as another string', () => {
     expect(anagrams('grape', 'pager')).toBe(true);
+    expect(anagrams('bob', 'rob')).toBe(false);
+    expect(anagrams('star', 'arts')).toBe(true);
   })
 })
