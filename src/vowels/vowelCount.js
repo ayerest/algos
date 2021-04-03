6776c51a2d60b279ade8a59dcd202f634c4b13f7
ayerest <irisiseve@gmail.com>
2021-04-03 10:27:10 -0700

    vowel count problem solved

--- src/vowels/vowelCount.js
+++ src/vowels/vowelCount.js
@@ -1,5 +1,7 @@
-const vowelCount = () => {
-  console.log("sup");
+const vowelCount = (string) => {
+  const vowels = /[aeiou]/gi;
+  const matches = string.match(vowels);
+  return matches?.length || 0;
 }
 
 export default vowelCount;
--- src/vowels/vowelCount.test.js
+++ src/vowels/vowelCount.test.js
@@ -1,8 +1,11 @@
+import { expect } from "@jest/globals";
 import vowelCount from "./vowelCount"
 
 describe('vowelCount method', () => {
   it('counts the number of vowels in a string', () => {
-    expect(vowelCount('string').toBe(1));
+    expect(vowelCount('string')).toBe(1);
+    expect(vowelCount('mississippi')).toBe(4);
+    expect(vowelCount('why')).toBe(0);
   })
 })
 
