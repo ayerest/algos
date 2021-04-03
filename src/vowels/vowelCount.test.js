760fe708cdd2ab100c102c13a780be3434907645
ayerest <irisiseve@gmail.com>
2021-04-03 10:20:32 -0700

    sup

--- src/vowels/vowelCount.test.js
+++ src/vowels/vowelCount.test.js
@@ -3,6 +3,7 @@ import vowelCount from "./vowelCount"
 describe('vowelCount method', () => {
   it('counts the number of vowels in a string', () => {
     expect(vowelCount('string').toBe(1));
+    epxect(vowelCount('butt').toBe(1));
   })
 })
 
