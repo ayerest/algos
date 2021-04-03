f1cf86b1fd3b89d73043650797e60622beb5d715
ayerest <irisiseve@gmail.com>
2021-04-03 10:20:47 -0700

    Revert "sup"
    This reverts commit 760fe708cdd2ab100c102c13a780be3434907645.

--- src/vowels/vowelCount.test.js
+++ src/vowels/vowelCount.test.js
@@ -3,7 +3,6 @@ import vowelCount from "./vowelCount"
 describe('vowelCount method', () => {
   it('counts the number of vowels in a string', () => {
     expect(vowelCount('string').toBe(1));
-    epxect(vowelCount('butt').toBe(1));
   })
 })
 
