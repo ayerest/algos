d1d3a74027bbe2e598587d3733ad8d08d990dea5
ayerest <irisiseve@gmail.com>
2021-04-10 10:33:45 -0700

    solved chunk problem

--- src/chunk/chunk.js
+++ src/chunk/chunk.js
@@ -1,5 +1,15 @@
-const chunk = () => {
-  console.log("sup")
+const chunk = (array, length) => {
+  let newArray = [];
+  let index = 0;
+  while (index < array.length) {
+    if (index + length > array.length) {
+      newArray.push(array.slice(index))
+    } else {
+      newArray.push(array.slice(index, index + length))
+    }
+    index += length;
+  }
+  return newArray;
 }
 
 export default chunk;
--- src/chunk/chunk.test.js
+++ src/chunk/chunk.test.js
@@ -3,6 +3,7 @@ import chunk from './chunk';
 
 describe('chunk method', () => {
   it('splits an array input into a list of arrays of given length', () => {
-    expect(chunk([1,2,3,4], 2)).toBe([[1,2], [3,4]]);
+    expect(chunk([1,2,3,4], 2)).toStrictEqual([[1,2], [3,4]]);
+    expect(chunk([1,2,3,4], 3)).toStrictEqual([[1,2,3], [4]]);
   })
 })
